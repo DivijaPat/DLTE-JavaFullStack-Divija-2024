4503bf381dbd42822fe3333e04f5ad4eaacfaf64
DivijaPat <divijapatwardhan@gmail.com>
2024-03-07 00:11:36 +0530

    added java programs

--- DLTE-Java/DLTE-java-002/src/main/java/basics/service/incomeTax.java
+++ DLTE-Java/DLTE-java-002/src/main/java/basics/service/incomeTax.java
@@ -4,67 +4,65 @@ import java.util.Scanner;
 
 public class incomeTax {
 
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        double salary=0.0;
-        System.out.println("enter your salary");
-        salary=scanner.nextDouble();
-        System.out.println("Enter your choice:\n1.Old Task Regime\n2.New Task Regime");
-        int choice=scanner.nextInt();
-        switch(choice){
-            case 1:
-                if(salary>0 && salary<250000)
-                    System.out.println("old tax rate is Exempted");
+    public static void main(String[] args){
+        double income,taxAmount,taxpercent=0;
+        String taxType="";
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter the income");
+        income=scanner.nextInt();
+        System.out.println("enter the type of incometax");
+        taxType=scanner.next();
+        switch(taxType){
+            case"OldRegime": case"oldregime": case"OLDREGIME":
+                if(income>=0 && income<=250000){
+                    System.out.println("no tax levied");
+                }
+                else if(income>250000 && income<=500000){
+                    taxpercent=0.05;
+                }
+                else if(income>500000 && income<=750000){
+                    taxpercent=0.2;
+                }
+                else if(income>750000 && income<=1000000){
+                    taxpercent=0.2;
+                }
+                else if(income>1000000 && income<=1250000) {
+                    taxpercent = 0.3;
 
-                else if(salary>=250000 && salary<500000)
-                    System.out.println("old tax rate is "+(0.05*salary));
-
-
-                else if(salary>=500000 && salary<750000)
-                    System.out.println("old tax rate is "+(0.20*salary));
-
-                else if(salary>=750000 && salary<1000000)
-                    System.out.println("old tax rate is "+(0.20*salary));
-
-                else if(salary>=1000000 && salary<1250000)
-                    System.out.println("old tax rate is "+(0.30*salary));
-
-                else if(salary>=1250000 && salary<1500000)
-                    System.out.println("old tax rate is "+(0.30*salary));
-
-                else
-                    System.out.println("old tax rate is "+(0.30*salary));
+                }
+                else if(income>1250000&& income<=1500000){
+                    taxpercent=0.3;
+                }
+                else{
+                    taxpercent=0.3;
+                }
                 break;
 
-            case 2:
-                if(salary>0 && salary<250000)
-                    System.out.println("new task rate is Exempted");
-
-                else if(salary>=250000 && salary<500000)
-                    System.out.println("new tax rate is "+(0.05*salary));
-
-
-                else if(salary>=500000 && salary<750000)
-                    System.out.println("new tax rate is "+(0.10*salary));
-
-                else if(salary>=750000 && salary<1000000)
-                    System.out.println("new tax rate is "+(0.15*salary));
-
-                else if(salary>=1000000 && salary<1250000)
-                    System.out.println("new tax rate is "+(0.20*salary));
-
-                else if(salary>=1250000 && salary<1500000)
-                    System.out.println("new tax rate is "+(0.25*salary));
-
-                else
-                    System.out.println("new tax rate is "+(0.30*salary));
-
-                break;
-
-            default:
-                System.exit(0);
+            case"NewRegime": case"newRegime": case"NEWREGIME":
+                if(income>=0 && income<=250000){
+                    System.out.println("no tax levied");
+                }
+                else if(income>250000 && income<=500000){
+                    taxpercent=0.05;
+                }
+                else if(income>500000 && income<=750000){
+                    taxpercent=0.1;
+                }
+                else if(income>750000 && income<=1000000){
+                    taxpercent=0.15;
+                }
+                else if(income>1000000 && income<=1250000){
+                    taxpercent=0.20;
+                }
+                else if(income>1250000&& income<=1500000){
+                    taxpercent=0.25;
+                }
+                else{
+                    taxpercent=0.3;
+                }
         }
-
+        taxAmount=(income/100)*taxpercent;
+        System.out.println("taxamount payable is"+taxAmount);
 
 
     }
