5beb6b149320dca9ac8126a3f695757fd01fbfc9
DivijaPat <divijapatwardhan@gmail.com>
2024-03-24 18:20:04 +0530

    added

---
+++ DLTE-Spring/DLTE-Spring-Tasks/springjdbc/jdbcTemplate/src/main/java/task/jdbctemplate/demo/controller/TransactionController.java
@@ -0,0 +1,38 @@
+package task.jdbctemplate.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import task.jdbctemplate.demo.entity.Transaction;
+import task.jdbctemplate.demo.services.TransactionServices;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+    @Autowired
+    TransactionServices transactionServices;
+
+    @PostMapping("/add")
+    public Transaction addTransaction(@RequestBody Transaction transaction){
+        Transaction transaction1=transactionServices.apiSave(transaction);
+        return transaction1;
+    }
+
+    @GetMapping("/send/{sender}")
+    public List<Transaction> findBySender(@PathVariable("sender") String sender){
+        return transactionServices.apiFindBySender(sender);
+    }
+
+    @GetMapping("/receive/{receiver}")
+    public List<Transaction> findByReceiver(@PathVariable("receiver") String receiver){
+        return transactionServices.apiFindByReceiver(receiver);
+    }
+
+    @GetMapping("/amount/{amount}")
+    public List<Transaction> findBySender(@PathVariable("amount") Integer amount){
+        return transactionServices.apiFindByAmount(amount);
+    }
+
+}
