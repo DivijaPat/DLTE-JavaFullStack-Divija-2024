425e0233890b69c07d53993ebb112eb000fd2599
DivijaPat <divijapatwardhan@gmail.com>
2024-03-24 18:36:41 +0530

    added

---
+++ DLTE-Spring/DLTE-Spring-Tasks/springdao/springdao/src/main/java/task/bootdao/springdao/SpringdaoApplication.java
@@ -0,0 +1,13 @@
+package task.bootdao.springdao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdaoApplication.class, args);
+    }
+
+}
---
+++ DLTE-Spring/DLTE-Spring-Tasks/springdao/springdao/src/test/java/task/bootdao/springdao/SpringdaoApplicationTests.java
@@ -0,0 +1,13 @@
+package task.bootdao.springdao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
